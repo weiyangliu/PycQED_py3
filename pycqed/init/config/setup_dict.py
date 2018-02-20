3926099723b4d504a66c7e9123110e136c08d5f9
elrama- <sagastizabal.ramiro@gmail.com>
2018-02-20 18:27:47 +0100

    setup_dict fixed

--- pycqed/init/config/setup_dict.py
+++ pycqed/init/config/setup_dict.py
@@ -18,20 +18,20 @@ mac_dict = {'203178706891063': 'CDickel_Desktop',
             '180725258210527': 'Niels_macbook',
             '109952948723616': 'Ramiro_Desktop',
             '215977245834050': 'Xiang_PC',
-            '23213':'Malay_Laptop',
+            '23213': 'Malay_Laptop',
             '31054844829911': 'Sjoerd_laptop',
-            '26830024075025': 'Qudev_testbench',
-            '88623634748008':'LaAprilia_1'
+            '26830024075025': 'Qudev_testbench'
             }
 
 data_dir_dict = {'tud276606_FPGA_PC': 'D:\Experiments/CBox_Testing/Data',
                  'CDickel_Desktop': 'D:\Experiments/ExperimentName/Data',
                  'Sjoerd_laptop': 'D:\data',
-                 'Malay_Laptop':'D:\Tomo datasets',
+                 'Malay_Laptop': 'D:\Tomo datasets',
                  'Adriaans_Macbook': ('/Users/Adriaan/Documents/Testing/Data'),
                  'Niels_macbook': '/Users/nbultink/temp_data',
-                 'La_Ferrari':  'D:\Experiments/1511_RabiSims2/Data',
-                 'TUD277449': 'D:\Experiments/1710_FlipchipS7/Data',
+                 # 'La_Ferrari':  'D:\Experiments/1511_RabiSims2/Data',
+                 'TUD277449': 'D:\Experiments/1801_QECVQE/Data',
+                 # 'TUD277449': 'D:\Experiments/1801_Intel_Ntone_W202/Data',
                  'La_Maserati_Jr': 'D:\\Experiments\\1610_QcodesTests\\Data',
                  'La_Maserati_JrJr': 'D:\\Experiments\\1702_Starmon\\data',
                  'La_Vespa': 'D:\\Experiments\\161111_LaVespa_Intel_HR\\Data',
@@ -40,13 +40,12 @@ data_dir_dict = {'tud276606_FPGA_PC': 'D:\Experiments/CBox_Testing/Data',
                  'La_Ducati': 'D:\Experiments/Simultaneous_Driving/Data',
                  'La_Ducati_Jr': 'D:\Experiments/1611_Starmon/Data',
                  'Simulation_PC': 'D:\Experiments/testSingleShotFidelityAnalysis/Data',
-                 'Ramiro_Desktop': r'D:\\Repositories\\PhD_RS\\data_local',
+                 # 'Ramiro_Desktop': r'D:\\Repositories\\PhD_RS\\data_local',
                  # 'Ramiro_Desktop': r'\\131.180.82.81\\Experiments\\1702_Starmon\\data',
+                 'Ramiro_Desktop': r'\\TUD277449\\Experiments\\1801_QECVQE\\Data',
                  # 'Ramiro_Desktop': r'\\131.180.82.81\\data',
                  # 'Ramiro_Desktop': r'\\131.180.82.190\\Experiments\\1611_Starmon\\Data',
                  r'Nathans_Laptop': 'D:/nlangford\My Documents\Projects\Rabi Model\Experiment_1504\Data',
                  'Bart_Laptop': 'C:\Experiments/NumericalOptimization/Data',
-                 'Qudev_testbench' : r'E:\Control software\data',
-                 'LaAprilia_1' : r'D:\Experiments\1802_IntelS7Airbridges'
-                 #'LaAprilia_1' : r'D:\Experiments\1802_Argon\Data'
+                 'Qudev_testbench': r'E:\Control software\data'
                  }
