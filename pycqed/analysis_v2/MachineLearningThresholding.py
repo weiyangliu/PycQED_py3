d55c89f443341e926ff4df1e94ec0ef9344106f4
elrama- <sagastizabal.ramiro@gmail.com>
2018-03-16 16:44:46 +0100

    Updated ML

--- pycqed/analysis_v2/MachineLearningThresholding.py
+++ pycqed/analysis_v2/MachineLearningThresholding.py
@@ -29,87 +29,85 @@ except ImportError as e:
 
 
 
-class MachineLearningThresholding():
+class MachineLearningThresholding(object):
 
-	def __init__(self, data_I, data_Q, target_classifier, auto=True, label='', timestamp=None,
-                 fig_format='png',
-                 q0_label='q0',
-                 q1_label='q1', close_fig=True, **kw):
+    def __init__(self, X_calibration, target_classifier, mesh_step_size, **kw):
 
 
-	self.X = np.zeros(len(data_I),2)
-	self.X[:,0] = data_I
-	self.X[:,1] = data_Q
-	self.y = training_classifier
+        self.X = X_calibration
+        self.y = target_classifier
+        self.h = mesh_step_size  # step size in the mesh
 
-	def train_classifier_on_calibration_point(self):
-		name = 'SVC'
-		clf = SVC(gamma=2, C=1)
+        self.names = ["RBF SVM"]
 
-		figure = plt.figure(figsize=(27, 9))
-		i = 1
-		# preprocess dataset, split into training and test part
-		self.X = StandardScaler().fit_transform(self.X)
-		self.X_train, self.X_test, self.y_train, self.y_test = \
-		    train_test_split(self.X, self.y, test_size=.4, random_state=42)
+        self.clf = SVC(gamma=2, C=1)
+    def train_classifier_on_calibration_point(self):
+        figure = plt.figure(figsize=(8, 8))
+        i = 1
+        # preprocess dataset, split into training and test part
+        self.X = StandardScaler().fit_transform(self.X)
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+                train_test_split(self.X, self.y, test_size=.4, random_state=42)
 
-		self.x_min, self.x_max = self.X[:, 0].min() - .5, self.X[:, 0].max() + .5
-		self.y_min, self.y_max = self.X[:, 1].min() - .5, self.X[:, 1].max() + .5
-		self.xx, self.yy = np.meshgrid(np.arange(self.x_min, self.x_max, h),
-		                     np.arange(self.y_min, self.y_max, h))
+        self.x_min, self.x_max = self.X[:, 0].min() - .5, self.X[:, 0].max() + 0.5
+        self.y_min, self.y_max = self.X[:, 1].min() - .5, self.X[:, 1].max() + 0.5
+        self.xx, self.yy = np.meshgrid(np.arange(self.x_min, self.x_max, self.h),
+                np.arange(self.y_min, self.y_max, self.h))
 
-		# just plot the dataset first
-		cm = plt.cm.RdBu
-		cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-		ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-		ax.set_title("Input data")
-		# Plot the training points
-		ax.scatter(self.X_train[:, 0], self.X_train[:, 1], c=self.y_train, cmap=cm_bright,
-		           edgecolors='k')
-		# and testing points
-		ax.scatter(self.X_test[:, 0], self.X_test[:, 1], c=self.y_test, cmap=cm_bright, alpha=0.6,
-		           edgecolors='k')
-		ax.set_xlim(self.xx.min(), self.xx.max())
-		ax.set_ylim(self.yy.min(), self.yy.max())
-		ax.set_xticks(())
-		ax.set_yticks(())
-		i += 1
-		ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-		clf.fit(X_train, y_train)
-		score = clf.score(X_test, y_test)
+        # just plot the dataset first
+        cm = plt.cm.RdBu
+        cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+        #len(datasets) hardcoded to be 3
+        ax = plt.subplot(3, 2, 1)
+        ax.set_title("Input data")
+        # Plot the training points
+        ax.scatter(self.X_train[:, 0],
+                   self.X_train[:, 1],
+                   c=self.y_train,
+                   cmap=cm_bright,
+                   edgecolors='k')
+        # and testing points
+        ax.scatter(self.X_test[:, 0], self.X_test[:, 1], c=self.y_test, cmap=cm_bright, alpha=0.6,
+            edgecolors='k')
+        ax.set_xlim(self.xx.min(), self.xx.max())
+        ax.set_ylim(self.yy.min(), self.yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        # iterate over classifiers
+        
+        ax = plt.subplot(3, 2, 1)
+        self.clf.fit(self.X_train, self.y_train)
+        score = self.clf.score(self.X_test, self.y_test)
 
-		# Plot the decision boundary. For that, we will assign a color to each
-		# point in the mesh [x_min, x_max]x[y_min, y_max].
-		if hasattr(clf, "decision_function"):
-		    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-		else:
-		    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        if hasattr(self.clf, "decision_function"):
+            self.Z = self.clf.decision_function(np.c_[self.xx.ravel(), self.yy.ravel()])
+        else:
+            self.Z = self.clf.predict_proba(np.c_[self.xx.ravel(), self.yy.ravel()])[:, 1]
 
-		# Put the result into a color plot
-		Z = Z.reshape(xx.shape)
-		ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+        # Put the result into a color plot
+        self.Z = self.Z.reshape(self.xx.shape)
+        ax.contourf(self.xx, self.yy, self.Z, cmap=cm, alpha=.8)
 
-		# Plot also the training points
-		ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright,
-		           edgecolors='k')
-		# and testing points
-		ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-		           edgecolors='k', alpha=0.6)
+        # Plot also the training points
+        ax.scatter(self.X_train[:, 0], self.X_train[:, 1], c=self.y_train, cmap=cm_bright,
+        edgecolors='k')
+        # and testing points
+        ax.scatter(self.X_test[:, 0], self.X_test[:, 1], c=self.y_test, cmap=cm_bright,
+        edgecolors='k', alpha=0.6)
 
-		ax.set_xlim(xx.min(), xx.max())
-		ax.set_ylim(yy.min(), yy.max())
-		ax.set_xticks(())
-		ax.set_yticks(())
-		ax.set_title(name)
-		ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-		        size=15, horizontalalignment='right')
-		i += 1
+        ax.set_xlim(self.xx.min(), self.xx.max())
+        ax.set_ylim(self.yy.min(), self.yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        ax.set_title('RBF SVM')
+        ax.text(self.xx.max() - .3, self.yy.min() + .3, ('%.2f' % score).lstrip('0'),
+        size=15, horizontalalignment='right')
+        
 
-		# plt.tight_layout()
-		# plt.show()
+        return self.clf
 
 
-	# plt.tight_layout()
-	# plt.show()
 
 
